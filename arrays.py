829ba3a21f26fe12fb62de874252e02816e4eb6c
P Supriya <42340028+supriyanivi@users.noreply.github.com>
2019-02-05 11:48:19 +0530

    Create arrays.py

---
+++ arrays.py
@@ -0,0 +1,7 @@
+number_array=list()
+number = input("enter the no.of elements you want:")
+print (" enter numbers in array:")
+for i in range(int(number)):
+n= input("number:")
+number_array.append(int(n))
+print("ARRAY:",number_array)
